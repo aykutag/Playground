e8b43a73b7db7f891437bf312e2a4c128d5eceef
anton <akropp@gmail.com>
2013-11-18 10:44:58 -0500

    adding zip test

--- java/enumerable/src/test/java/EnumerableTests.java
+++ java/enumerable/src/test/java/EnumerableTests.java
@@ -1,4 +1,5 @@
 import com.devshorts.enumerable.Enumerable;
+import com.devshorts.enumerable.data.Tuple;
 import org.junit.Test;
 
 import java.util.List;
@@ -14,5 +15,14 @@ public class EnumerableTests {
                                       .toList();
 
         assertEquals(ids, asList(1,2,3,4,5,6,7));
+    }
+
+    @Test
+    public void Zip(){
+        Enumerable<Integer> ids = Enumerable.init(asList(1, 2, 3, 4, 5));
+
+        List<String> expectZipped = asList("11", "22", "33", "44", "55");
+
+        assertEquals(expectZipped, ids.zip(ids, (f, s) -> f.toString() + s.toString()).toList());
     }
 }
