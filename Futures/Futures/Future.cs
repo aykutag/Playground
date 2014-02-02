49e8a9a73d18a8b62390ba351ec0d4a4934b06b5
devshorts <akropp@gmail.com>
2014-02-01 18:27:20 -0800

    adding futures project

---
+++ Futures/Futures/Future.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Threading;
+
+namespace Future
+{
+    public class Future<T>
+    {
+        private bool _isComplete;
+
+        private Thread _runner;
+
+        private Exception _ex;
+
+        private readonly object _lock = new object();
+
+        private T _result;
+
+        public Future(Func<T> function)
+        {
+            Execute(Wrapped(function));
+        }
+
+        private Action Wrapped(Func<T> function)
+        {
+            return () =>
+            {              
+                try
+                {
+                    _result = function();
+
+                    lock (_lock)
+                    {
+                        _isComplete = true;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _ex = ex;
+                }                
+            };
+        }
+
+        private void Execute(Action wrapped)
+        {
+            _runner = new Thread(new ThreadStart(wrapped));
+    
+            _runner.Start();
+        }
+
+        public T Resolve()
+        {
+            lock (_lock)
+            {
+                if (_isComplete)
+                {
+                    return _result;
+                }
+            }
+
+            _runner.Join();
+                
+            if (_ex != null)
+            {
+                throw _ex;
+            }
+
+            return _result;        
+        }
+
+        public Future<T> Then(Func<T> next)
+        {
+            return new Future<T>(() =>
+            {
+                Resolve();
+
+                return next();
+            });
+        }  
+
+        public Future<Y> Then<Y>(Func<T, Y> next)
+        {
+            return new Future<Y>(() =>
+            {
+                var previousResult = Resolve();
+
+                return next(previousResult);
+            });
+        }  
+    }
+}
---
+++ Futures/Futures/FutureTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+
+namespace Future
+{
+    [TestFixture]
+    public class FutureTests
+    {
+        [Test]
+        public void TestFuture()
+        {
+            int count = 0;
+         
+            Func<int> action = () =>
+            {
+                Console.WriteLine("Running " + count);
+                Thread.Sleep(TimeSpan.FromMilliseconds(count * 100));
+                Console.WriteLine("Resolving " + count);
+
+                count ++;
+                return count;
+            };
+
+            var future = new Future<int>(action).Then(action).Then(action);
+
+            Console.WriteLine("All setup, nonblock but now wait");
+
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+
+            Console.WriteLine("Requesting result");
+
+            var result = future.Resolve();
+
+            Assert.AreEqual(3, result);
+        }
+
+        [Test]
+        [ExpectedException(typeof(Exception))]
+        public void TestException()
+        {
+            Func<int> action = () =>
+            {
+                throw new Exception("Error");
+            };
+
+            var future = new Future<int>(action);
+
+            future.Resolve();
+        }
+    }
+}
