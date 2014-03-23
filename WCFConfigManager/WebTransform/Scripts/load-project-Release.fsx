3d6b43a8233fc06a365f596ec06ac59816e8fbcd
Anton Kropp <akropp@gmail.com>
2014-03-22 17:53:35 -0700

    loading wcf transform code

---
+++ WCFConfigManager/WebTransform/Scripts/load-project-Release.fsx
@@ -0,0 +1,2 @@
+#load @"C:\Projects\personal\Playground\WCFConfigManager\WebTransform\Scripts\load-refs-Release.fsx"
+#r @"C:\Projects\personal\Playground\WCFConfigManager\WebTransform\bin\Release\WebTransform.exe"
---
+++ WCFConfigManager/WebTransform/Scripts/load-project.fsx
@@ -0,0 +1,2 @@
+#load @"C:\Projects\personal\Playground\WCFConfigManager\WebTransform\Scripts\load-refs.fsx"
+#r @"C:\Projects\personal\Playground\WCFConfigManager\WebTransform\bin\Debug\WebTransform.exe"
