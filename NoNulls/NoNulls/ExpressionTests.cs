de49c83f8103d7162a0b46473f6495c9a5f6949b
Anton Kropp <akropp@gmail.com>
2014-03-29 17:16:18 -0700

    adding option expression to no nulls

---
+++ NoNulls/NoNulls/ExpressionTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace NoNulls
+{
+    [TestClass]
+    public class ExpressionTests
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var user = new User();
+
+            var name = Option.Safe(() => user.School.District.Street.Name);
+
+            Assert.IsNull(name);
+        }
+
+
+        [TestMethod]
+        public void Test2()
+        {
+            var user = new User
+                       {
+                           School = new School()
+                       };
+
+            var name = Option.Safe(() => user.School.District);
+
+            Assert.IsNull(name);
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            var user = new User
+            {
+                School = new School
+                         {
+                             District = new District()
+                         }
+            };
+
+            var name = Option.Safe(() => user.School.District);
+
+            Assert.IsNotNull(name);
+        }
+    }
+}
---
+++ NoNulls/NoNulls/Option.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace NoNulls
+{
+    public class Option
+    {
+        public static T Safe<T>(Expression<Func<T>>  input)
+        {
+            var transform = new NullVisitor().VisitAndConvert(input, "test");
+
+            return transform.Compile()();
+        }
+    }
+
+    public class NullVisitor : ExpressionVisitor
+    {
+        private Stack<Expression> expressions = new Stack<Expression>();
+
+        private Expression finalExpression;
+
+        private static Random rand = new Random();
+
+        protected override Expression VisitLambda<T>(Expression<T> node)
+        {
+            var result = base.VisitLambda<T>(node);
+
+            return result;
+        }
+
+        protected override Expression VisitMember(MemberExpression node)
+        {            
+            expressions.Push(node);
+
+            if (finalExpression == null)
+            {
+                finalExpression = node;
+            }
+
+            var exp = Visit(node.Expression);
+
+            if (expressions.Count == 0)
+            {
+                return exp;
+            }
+
+            var condition =  BuildIfs(expressions.Pop());
+
+            return Expression.Block(new []{ condition} );
+        }
+
+        private LabelTarget Label
+        {
+            get { return Expression.Label(finalExpression.Type); }
+        }
+
+        private Expression BuildIfs(Expression top)
+        {
+            var returnNull = Expression.Constant(null, finalExpression.Type);//Expression.Return(Expression.Label(), finalExpression.Type));
+
+            var ifNull = Expression.ReferenceEqual(top, Expression.Constant(null));
+
+            var finalReturn = finalExpression;//Expression.Return(Label, finalExpression, finalExpression.Type);
+
+            var condition = Expression.Condition(ifNull, returnNull, expressions.Count == 0 ? finalReturn : BuildIfs(expressions.Pop()));
+
+            return condition;            
+        }
+    }
+}
